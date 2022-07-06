67119a628226fb3c78ba0635e50f690a3321a3d2
bekirakca49 <bekirakca0@gmailc.om>
2022-07-07 00:29:15 +0200

    bekir5

--- src/proje2.java
+++ src/proje2.java
@@ -2,5 +2,6 @@ public class proje2 {
     public static void main(String[] args) {
         System.out.println("bekir akca projeye dahil oldu");
         System.out.println("bekir akça düzenleme yaptı");
+        System.out.println("bekir2");
     }
 }
