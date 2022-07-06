2922edf7ee6b1350d7f44778544d056455efb9bd
bilalMavus <bilalmavuss@gmail.com>
2022-07-07 01:01:02 +0300

    new 3

--- src/bilal1.java
+++ src/bilal1.java
@@ -2,5 +2,6 @@ public class bilal1 {
     public static void main(String[] args) {
         System.out.println("Merhab ben Bilal");
         System.out.println("Merhab ben Bilal2");
+        System.out.println("Merhab ben Bilal3");
     }
 }
