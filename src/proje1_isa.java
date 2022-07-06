b4c6168dfe3e46488c551937fbea03703c548027
isasentr <isasentr@gmail.com>
2022-07-07 01:01:15 +0300

    Test isa 3

--- src/proje1_isa.java
+++ src/proje1_isa.java
@@ -3,6 +3,7 @@ public class proje1_isa {
         System.out.println("Mrb");
         System.out.println("Ben isa...");
         System.out.println("Sdet1");
+        System.out.println("Sdet2");
 
     }
 }
