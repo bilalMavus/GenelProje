7d996e1af84c1f623951a4e9b71bf6b3824a38e0
boraakinyilmaz <boraakinyilmaz@hotmail.com>
2022-07-07 01:05:05 +0300

    commit deneme2

--- src/borabora.java
+++ src/borabora.java
@@ -2,5 +2,6 @@ public class borabora {
     public static void main(String[] args) {
         System.out.println("borabora");
         System.out.println("commit deneme2");
-    }
+
+        System.out.println("request deneme");}
 }
