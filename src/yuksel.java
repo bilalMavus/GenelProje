14f50933635c600e5a775b80b5c78dfcc22eb87c
yukselyucesan <yukselyucesan99@hotmail.com>
2022-07-07 01:07:32 +0300

    ykl3

--- src/yuksel.java
+++ src/yuksel.java
@@ -2,5 +2,6 @@ public class yuksel {
     public static void main(String[] args) {
         System.out.println("merhana ben yuksel");
         System.out.println("merhana ben yuksel2");
+        System.out.println("merhana ben yuksel3");
     }
 }
