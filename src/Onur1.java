b432ce176baec0eacf20f2e8856759f90bcbd74c
OnurIncdyi <108151328+OnurIncdyi@users.noreply.github.com>
2022-07-07 01:01:10 +0300

    Onur3

--- src/Onur1.java
+++ src/Onur1.java
@@ -2,5 +2,7 @@ public class Onur1 {
     public static void main(String[] args) {
         System.out.println("Merhaba Ben Onur");
         System.out.println("Yeni 00:57");
+        System.out.println("Yeni 00:57");
+
     }
 }
